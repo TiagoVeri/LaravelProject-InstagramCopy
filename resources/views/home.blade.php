9d703549b349018b2629d6ea168e12e339607548
TiagoAlessandro <tiago.veri@mult-e.com.br>
2019-11-14 11:06:47 -0300

    First commit, added auth, customized home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://pbs.twimg.com/profile_images/782132102368223232/yJxwuI1g_400x400.jpg"
+            class="rounded-circle  img-fluid"  style="height: 110px;"/>
+        </div>
+        <div class="col-9 pt-5">
+          <div><h1>freecodecamp</h1></div>
+          <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+          </div>
+          <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+          <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie</div>
+          <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row">
+      <div class="col-4">
+          <img src="https://www.desmaakspecialist.nl/wp-content/uploads/sites/2/2017/04/IMG-20170401-WA0004-300x300.jpg" />
+      </div>
+      <div class="col-4">
+          <img src="https://www.desmaakspecialist.nl/wp-content/uploads/sites/2/2017/04/IMG-20170401-WA0004-300x300.jpg" />
+      </div>
+      <div class="col-4">
+          <img src="https://www.desmaakspecialist.nl/wp-content/uploads/sites/2/2017/04/IMG-20170401-WA0004-300x300.jpg" />
+      </div>
+    </div>
+</div>
+@endsection
