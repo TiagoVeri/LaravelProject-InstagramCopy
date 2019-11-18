61a5f8a439e5544cc1aaa246ddf90fd4a13694f2
TiagoAlessandro <tiago.veri@mult-e.com.br>
2019-11-18 09:42:51 -0300

    Added Profiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,9 +14,9 @@
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
           </div>
-          <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-          <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centurie</div>
-          <div><a href="#">www.freecodecamp.org</a></div>
+          <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+          <div>{{$user->profile->description}}</div>
+          <div><a href="#">{{$user->profile->url}}</a></div>
         </div>
     </div>
 
