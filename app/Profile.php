c0fa739fc9a77824d227641c6c1d703206e70dc9
TiagoAlessandro <tiago.veri@mult-e.com.br>
2019-11-19 15:32:57 -0300

    Added FollowsController, improved avatar image conection, added table ManyToMany, started to add the follow function, some routes

--- app/Profile.php
+++ app/Profile.php
@@ -8,8 +8,19 @@ use App\User;
 class Profile extends Model
 {
     protected $guarded = [];
-    
+
+    public function profileImage(){
+
+      $imagePath = ($this->image) ? $this->image : 'profile/XH8CfoJJhsLCoovAhOcuAHD1ZBGh8erpxDjKI0bt.jpeg';
+      return '/storage/'. $imagePath;
+    }
+
+    public function followers(){
+      return $this->belongsToMany(User::class);
+      
+    }
     public function user(){
       return $this->belongsTo(User::class);
     }
+
 }
